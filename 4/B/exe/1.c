5cc65f12dfb7f5aeaf3c8979862ea37069d30d5d
Aadish5565 <aphade46@gmail.com>
2026-08-14 09:50:03 +0530

    Add binary search implementation for strings

---
+++ 4/B/exe/1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char a[100][50], key[50];
+    int n, i, low, high, mid, found = 0;
+
+    printf("Enter number of strings: ");
+    scanf("%d", &n);
+
+    printf("Enter strings in descending order:\n");
+    for(i = 0; i < n; i++)
+        scanf("%s", a[i]);
+
+    printf("Enter string to search: ");
+    scanf("%s", key);
+
+    low = 0;
+    high = n - 1;
+
+    while(low <= high)
+    {
+        mid = (low + high) / 2;
+
+        if(strcmp(a[mid], key) == 0)
+        {
+            found = 1;
+            break;
+        }
+        else if(strcmp(a[mid], key) < 0)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+
+    if(found)
+        printf("String found at position %d", mid + 1);
+    else
+        printf("String not found");
+
+    return 0;
+}
